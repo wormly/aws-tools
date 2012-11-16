592a3f9d6510ac88f3dfe7b17b676f104e185e45
roddik <roddiku@gmail.com>
2012-11-16 20:00:21 +0200

    Added iam agent

---
+++ delete-policy.js
@@ -0,0 +1,25 @@
+
+var argv = require('optimist').argv;
+var awssum = require('awssum');
+var amazon = awssum.load('amazon/amazon');
+var Iam = awssum.load('amazon/iam').Iam;
+
+var Retrier = require('./lib/retrier.js');
+var IamAgent = require('./lib/iamagent.js');
+
+var iam = new Iam({
+	accessKeyId : argv.key || process.env.AWS_KEY,
+	secretAccessKey : argv.secret || process.env.AWS_SECRET
+});
+
+var agent = new IamAgent(iam);
+var retrier = new Retrier(argv.attempts || 5);
+
+retrier.run(function(callback) {
+	agent.deletePolicy({
+		policyName: argv.policyName,
+		userName: argv.userName
+	}, callback);
+}, function(err, data) {
+	if (err) console.log("Error:", err);
+});
