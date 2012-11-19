ef3421432c35451d4adcf6a45e31e41090291516
roddik <roddiku@gmail.com>
2012-11-19 11:28:27 +0200

    Added EIP agent

---
+++ remapeip.js
@@ -0,0 +1,29 @@
+
+var argv = require('optimist').argv;
+
+var awssum = require('awssum');
+var amazon = awssum.load('amazon/amazon');
+var Ec2 = awssum.load('amazon/ec2').Ec2;
+
+var Retrier = require('./lib/retrier.js');
+var EIPAgent = require('./lib/eipagent.js');
+var InstanceData = require('./lib/instancedata.js');
+
+var instanceDataGetter = new InstanceData(require('request'));
+
+instanceDataGetter.getRegion(function(err, region) {
+	var ec2 = new Ec2({
+		accessKeyId : argv.key || process.env.AWS_KEY,
+		secretAccessKey : argv.secret || process.env.AWS_SECRET,
+		region: region
+	});
+
+	var agent = new EIPAgent(ec2);
+	agent.remapEIP({
+		ip: argv.ip
+	}, function(err) {
+		if (err) {
+			console.log(err);
+		}
+	});
+});
---
+++ spec/eipagent.spec.js
@@ -0,0 +1,31 @@
+
+require('./utils.js');
+
+describe('EIP Agent', function() {
+	var EIPAgent = require('../lib/eipagent.js');
+
+	var agent, ec2, ip = '1.2.3.4', data;
+
+	beforeEach(function() {
+		ec2 = stub('AssociateAddress');
+		data = stub('getInstanceId');
+		agent = new EIPAgent(ec2, data);
+	});
+
+	it('remaps', function() {
+		var cb = jasmine.createSpy();
+
+		agent.remapEIP({
+			ip: ip
+		}, cb);
+
+		expect(data.getInstanceId).toHaveBeenCalled();
+		data.getInstanceId.mostRecentCall.args[0](null, 'instance');
+
+		expect(ec2.AssociateAddress.mostRecentCall.args[0]).toEqual({ PublicIp : ip, InstanceId : 'instance' });
+
+		ec2.AssociateAddress.mostRecentCall.args[1]('err', 'data');
+
+		expect(cb).toHaveBeenCalledWith('err', 'data');
+	});
+});
