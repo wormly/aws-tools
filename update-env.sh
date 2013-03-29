a414093c481496f02b4e5b1241971b182a657ab0
roddik <roddiku@gmail.com>
2013-03-29 14:10:03 +0200

    Silenced curl

--- update-env.sh
+++ update-env.sh
@@ -1,4 +1,4 @@
 
-export AWS_INSTANCE=`curl http://169.254.169.254/latest/meta-data/instance-id`
-export AWS_REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
-export AWS_AZ=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone`
+export AWS_INSTANCE=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+export AWS_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+export AWS_AZ=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
