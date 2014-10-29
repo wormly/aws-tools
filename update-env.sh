02dcfabb7c85aaec3afedcf882a7274951def9ae
Rodion Vynnychenko <roddiku@gmail.com>
2014-10-29 11:09:25 +0200

    dummy change 2

--- update-env.sh
+++ update-env.sh
@@ -5,3 +5,4 @@
 export AWS_INSTANCE=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 export AWS_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
 export AWS_AZ=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+
