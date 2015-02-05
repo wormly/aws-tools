816f1755bb6a38f6c920a3c134051d6f863bd880
Rodion Vynnychenko <roddiku@gmail.com>
2015-02-05 23:32:13 +0200

    Region overridable from command line

--- update-config.js
+++ update-config.js
@@ -1,10 +1,11 @@
 
 var AWS = require('aws-sdk');
+var argv = require('optimist').argv;
 
 AWS.config.update({
 	accessKeyId: process.env.AWS_KEY,
 	secretAccessKey: process.env.AWS_SECRET,
-	region: process.env.AWS_REGION,
+	region: argv.region || process.env.AWS_REGION,
 	httpOptions: {
 		timeout: 300000
 	}
